35bff22b196c3429db9010a7864e42af2b4ba3b5
Hugo2999 <hugostoso2641@gmail.com>
2024-04-11 11:43:51 -0300

    SETUP FEITO E TESTADO

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "Hugo2999"
+git config --global user.email "hugostoso2641@gmail.com"
+
+cd C:\Users\VICTORHUGOLOPESTAFFU\
+
+git clone https://github.com/Hugo2999/Caesar-Cipher.git
+cd Caesar-Cipher
+
+code .
